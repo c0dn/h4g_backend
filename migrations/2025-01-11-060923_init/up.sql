6ae4be912ddb57407bc0be49453cac6f1d1cecb9
c0dn <19631981+c0dn@users.noreply.github.com>
2025-01-12 21:41:52 +0800

    Fix user schema Order matters

--- migrations/2025-01-11-060923_init/up.sql
+++ migrations/2025-01-11-060923_init/up.sql
@@ -2,14 +2,14 @@
 CREATE SCHEMA IF NOT EXISTS private;
 GRANT ALL PRIVILEGES ON SCHEMA private TO "testuser";
 
-CREATE TYPE private.account_type AS ENUM ('user', 'admin', 'superadmin');
+CREATE TYPE private.account_type AS ENUM ('user', 'admin', 'super_admin');
 
 CREATE TABLE private.users (
     uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username TEXT NOT NULL,
     name TEXT NOT NULL,
-    password TEXT NOT NULL,
     phone TEXT NOT NULL,
+    password TEXT NOT NULL,
     email TEXT NOT NULL,
     role private.account_type NOT NULL
 );
